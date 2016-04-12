55cc43ac700934756c4e68d95a86950fadd3bfb2
Lina Felsner <he27tyhy@stud.informatik.uni-erlangen.de>
2016-04-12 11:32:27 +0200

    package Lina first commit

---
+++ src/edu/stanford/rsl/tutorial/Lina/Phantom.java
@@ -0,0 +1,14 @@
+package edu.stanford.rsl.tutorial.Lina;
+
+import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+
+public class Phantom extends Grid2D{
+
+	public Phantom(Grid2D input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
